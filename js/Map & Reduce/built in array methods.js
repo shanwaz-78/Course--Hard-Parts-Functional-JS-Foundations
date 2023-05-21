e7b906b25c1fe584993802cdf3b35ab9125fb98d
shanwaz-78 <mohammed.shanwaz78@gmail.com>
2023-05-21 11:46:09 +0530

    initial commit

---
+++ js/Map & Reduce/built in array methods.js
@@ -0,0 +1,7 @@
+const arr = [1,2,3];
+arr.push(4); // Where does he push came from.
+
+// First he will look for the arr he found it then we used dot notation the javaScript will make an object and insert that all methods in an object and every array has access to that object and he will create __proto__ property in arr Array and pass that reference to that object in which our all methods are exists.
+
+
+// what is shared object,, In javaScript every methods of like Array's Strings Objects they all methods stored in an object and every Array's Strings Objects has access to that so that object which is shared that's called shared object.
